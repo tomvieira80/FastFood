9d3d03d05613fce98af61f0bf96110843269ffcc
acarvalhopinto <andrecarvalhopinto@gmail.com>
2024-10-11 20:08:32 -0300

    Implementação de cliente

--- Repository/ClienteRepository.cs
+++ Repository/ClienteRepository.cs
@@ -18,7 +18,7 @@ namespace Repository
             _postgresContext = postgresContext ?? throw new ArgumentNullException(nameof(_postgresContext));
         }
 
-        public async Task IncluiClienteAsync(Cliente cliente)
+        public async Task IncluiCliente(Cliente cliente)
         {
             if (cliente == null)
                 throw new ArgumentNullException(nameof(cliente));
@@ -27,5 +27,56 @@ namespace Repository
             await _postgresContext.SaveChangesAsync();
         }
 
+
+        public async Task EditaCliente(Cliente cliente)
+        {
+            if (cliente == null)
+                throw new ArgumentNullException(nameof(cliente));
+
+            _postgresContext.Cliente.Update(cliente);
+            await _postgresContext.SaveChangesAsync();
+        }
+
+        public async Task RemoveCliente(Cliente cliente)
+        {
+            if (cliente == null)
+                throw new ArgumentNullException(nameof(cliente));
+
+            _postgresContext.Cliente.Remove(cliente);
+            await _postgresContext.SaveChangesAsync();
+        }
+
+        public async Task<Cliente> RecuperaClientePorId(Guid id)
+        {
+            Cliente cliente =
+                _postgresContext.Cliente.Where(c => c.Id.Equals(id)).FirstOrDefault();
+
+            if (cliente == null)
+                throw new ArgumentNullException(nameof(cliente));
+
+            return cliente;
+        }
+
+        public async Task<Cliente> RecuperaClientePorCPF(string cpf)
+        {
+            Cliente cliente =
+                 _postgresContext.Cliente.Where(c => c.CPF.Equals(cpf)).FirstOrDefault();
+
+            if (cliente == null)
+                throw new ArgumentNullException(nameof(cliente));
+
+            return cliente;
+        }
+
+        public async Task<List<Cliente>> RecuperaClientes()
+        {
+            List<Cliente> clientes =
+                 _postgresContext.Cliente.ToList();
+
+            if (clientes == null)
+                throw new ArgumentNullException(nameof(clientes));
+
+            return clientes;
+        }
     }
 }
